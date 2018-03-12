9bf1050570ddd4e47debdd95a2360491b18d3f96
oheaney380 <37104157+oheaney380@users.noreply.github.com>
2018-03-12 17:11:37 +0000

    IR and Movement Headers
    Very basic at the moment but it's a start; starting to think about the algorithm which we'll use to find the beacon.

---
+++ IR_Reading.c
@@ -0,0 +1,3 @@
+#include "IR_Reading.h"
+#include <xc.h>
+
---
+++ IR_Reading.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   IR_Reading.h
+ * Author: guilds
+ *
+ * Created on 12 March 2018, 16:37
+ */
+
+#ifndef IR_READING_H
+#define	IR_READING_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+unsigned char grabIR(void);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* IR_READING_H */
+
